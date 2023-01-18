05a6de88b51e0c3a56903c875b73681b4db48267
NataliaIakovleva-QA <natalia_yakovleva@yahoo.com>
2023-01-18 21:13:02 +0300

    second commit

--- battleship.js
+++ battleship.js
@@ -1,5 +1,10 @@
+function isNumeric(str) {
+     if (typeof str != "string") return false   
+     return !isNaN(str) && 
+            !isNaN(parseInt(str)) 
+   }
 var randomLoc = Math.floor(Math.random() * 5); 
-var location1 = randomLoc;    
+var location1 = randomLoc+1;    
 var location2 = location1 + 1;
 var location3 = location2 + 1;
 var guess; 
@@ -8,11 +13,14 @@ var guesses = 0;
 var isSunk = false;
 
 while (isSunk == false && guesses<6) {
-        guess = prompt ("Ready, aim, fire (enter a number 0-6):");
-        if (guess < 0 || guess > 6) {
+        guess = prompt ("Ready, aim, fire (enter a number 1-7):");
+        guesses = guesses +1;
+        if (!isNumeric(guess))
+        alert("Please enter a valid cell number!");
+        else
+        if (guess < 1 || guess > 7) {
              alert("Please enter a valid cell number!");
         } else { 
-             guesses = guesses +1;
             if (guess == location1 || guess == location2 || guess == location3) {
                   alert("HIT!")
                   hits = hits+1;
@@ -20,11 +28,13 @@ while (isSunk == false && guesses<6) {
                       isSunk = true;
                       alert ("You sank my battleship!");
                   }
-                  if (guess == location1) location1=-1
-                  else if (guess == location2) location2=-1
-                  else location3=-1
+                  if (guess == location1) location1=-location1
+                  else if (guess == location2) location2=-location2
+                  else location3=-location3
                } else {
-                     alert("MISS");
+                    if (guess == -location1 || guess == -location2 || guess == -location3) {
+                         alert("ALREADY HIT");} else  
+                         alert("MISS");
                }
            }
 }
